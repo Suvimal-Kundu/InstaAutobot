c36172e3642b918df3544a072b9c4c7f61f7ccaf
Suvimal Kundu <70266779+Suvimal-Kundu@users.noreply.github.com>
2020-11-18 22:46:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from instapy import InstaPy
 
-session = InstaPy(username="lovencare_puppy", password="luvuzindgi@1525")
+session = InstaPy(username="______________", password="_________________")
 session.login()
 
 #following section
@@ -9,7 +9,7 @@ session.set_do_follow(True, percentage=0, times=1)
 
 #comment section
 session.set_do_comment(True, percentage=50)
-session.set_comments([u'I love your post @{}! . Take time to view mine too.'])
+session.set_comments([u'_________________'])
 
 #like section
 session.like_by_tags(['germanshepherd'], amount=500)
